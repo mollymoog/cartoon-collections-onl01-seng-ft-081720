91a5b1112a99a32499627e383c521699f0da46fe
molly mk <mmkover@gmail.com>
2020-08-22 17:08:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,8 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.each do |item|
-    item === cheese_types
+  if ingredients.include?(cheese_types)
+    
   end
 
 end
